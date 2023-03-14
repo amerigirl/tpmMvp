a99753bbd829af576c98737aa52558e8b797f297
Be-Asia McKerracher <beasia.mckerracher@hrblock.com>
2023-03-14 16:33:27 -0500

    goofing off with the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeacherPortalComponent } from './teacher-portal/teacher-portal.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landingComponent', component: LandingComponent },
+  { path: 'teacherPortalComponent', component: TeacherPortalComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
