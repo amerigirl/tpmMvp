06af118fe791d7a8a7b496d08170b05ccaab557e
Be-Asia McKerracher <beasia.mckerracher@hrblock.com>
2023-03-15 13:01:21 -0500

    making it through the create form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TeacherPortalComponent } from './teacher-portal/teacher-portal.component';
 import { LandingComponent } from './landing/landing.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'teacherPortal', component: TeacherPortalComponent },
+  { path: 'editProfile', component: EditProfileComponent },
+  { path: 'createProfile', component: CreateProfileComponent },
+  { path: 'viewProfile', component: ViewProfileComponent },
+
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LandingComponent, TeacherPortalComponent]
+
+export const routingComponents = [
+  LandingComponent, 
+  TeacherPortalComponent, 
+  EditProfileComponent,
+  CreateProfileComponent,
+  ViewProfileComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-  routingComponents
+  routingComponents,
+  CreateProfileComponent,
+  ViewProfileComponent,
+  EditProfileComponent
   ],
   imports: [
     BrowserModule,
