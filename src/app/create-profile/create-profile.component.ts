c704404e8f9e31c7a700a906a69575c8e06508c7
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-03-17 17:55:36 -0500

    last save of the night

--- src/app/create-profile/create-profile.component.ts
+++ src/app/create-profile/create-profile.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 
 @Component({
@@ -10,17 +11,54 @@ import { HttpClient } from '@angular/common/http';
 
 
 
-export class CreateProfileComponent {
+export class CreateProfileComponent implements OnInit {
 
   constructor(private http: HttpClient){
-    
+
   }
 
   onProfileCreate(newProfile: { name: string, address: string, city:string, state:string,
                               zipcode:number, phone:string, email:string, location: string}) {
-    console.log(newProfile);
-    this.http.post('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles.json', newProfile)
+    //console.log(newProfile);
+
+    const headers = new HttpHeaders({'myHeader': 'profile'});
+
+    this.http.post('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles.json', newProfile, {headers: headers})
     .subscribe((res) => {
+      console.log(res)
+    })
+
+  }
+
+
+  //call the fetch method from ngOnInit method
+  //you call it here because when the page loads, it will then list all the things in the database
+
+  ngOnInit(){
+  this.fetchProfiles();
+  }
+
+  onProfilesFetch(){  //why do we need this method
+  this.fetchProfiles();
+  }
+
+  private fetchProfiles(){
+    this.http.get('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles.json')
+
+    /*
+      the pipe transforms the response so that it is more readable in the console
+    */
+    .pipe(map((res: any)=>{
+      const profiles = [];
+
+        for(const key in res) {
+          if(res.hasOwnProperty(key))
+          profiles.push({...res[key], id: key})
+        }
+        return profiles;
+      }))
+
+      .subscribe((res)=>{
       console.log(res)
     })
 
---
+++ src/app/model/products.ts
@@ -0,0 +1,15 @@
+export class Profile {
+
+  name?: string;
+  address?: string;
+  city?:string;
+  state?:string;
+  zipcode?:number;
+  phone?:string;
+  email?:string;
+  location?: string;
+  id?: string;
+
+
+
+}
