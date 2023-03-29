a6b403b8d26fba52358db98eae275b1dc128e530
Be-Asia McKerracher <beasia.mckerracher@hrblock.com>
2023-03-29 10:19:07 -0500

    fixed ngModel issue

--- src/app/Service/profiles.service.ts
+++ src/app/Service/profiles.service.ts
@@ -12,14 +12,21 @@ export class ProfileService{
   //create profile in the database
   createProfile(newProfile: { Fname: string, Mname: string, Lname: string, Taddress: string, TemailAddress:string, Tlocation: string, Tstandard:string}){
 
-    const headers = new HttpHeaders({'myHeader': 'profile'});
+    const headers = new HttpHeaders({
+      'myHeader': 'profile',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+    });
 
     //this post request takes 3 params
     this.http.post<{name: string}>(
-    'https://mvptpm-61807-default-rtdb.firebaseio.com/profiles.json',
-    newProfile, {headers: headers})
-    .subscribe((res) => {
-      console.log(res)
+     'https://mvptpm-61807-default-rtdb.firebaseio.com/profiles.json',
+      newProfile, {headers: headers})
+      .subscribe((res) => {
+      console.log(res)  
+      
+     
     });
 
   }
@@ -28,9 +35,11 @@ export class ProfileService{
   //<{[key: string]: Profile}> sets the structure for the observablet that returns
 
   fetchProfile(){
-    return this.http.get<{[key: string]: Profile}>('https://localhost:7142/api/Students')
+    return this.http.get<{[key: string]: Profile}>('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles.json')
     .pipe(map((res: any)=>{
+
     const profiles = [];
+      
       //loops through keys in the response, returns matches from the server
       for(const key in res) {
         if(res.hasOwnProperty(key)) //this is how you check if a property is in an object
@@ -43,13 +52,13 @@ export class ProfileService{
   }
 
   updateProfile(id: string, value: Profile){
-    this.http.put('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles/' + id +'.json', value)
+    this.http.put('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles' + id +'.json', value)
     .subscribe();
 
   }
 
   deleteProfile(id: string){
-    this.http.delete('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles/' + id +'.json')
+    this.http.delete('https://mvptpm-61807-default-rtdb.firebaseio.com/profiles' + id +'.json')
     .subscribe();
   }
 }
